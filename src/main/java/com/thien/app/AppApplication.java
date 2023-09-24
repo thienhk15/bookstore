7525586a3a5c3b35b322995ca317e00bb6d68bc2
thien <79550249+thienhk15@users.noreply.github.com>
2023-09-24 21:24:57 +0700

    Update AppApplication.java

--- src/main/java/com/thien/app/AppApplication.java
+++ src/main/java/com/thien/app/AppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class AppApplication {
-	//test
+	//testt
 	public static void main(String[] args) {
 		//test
 		SpringApplication.run(AppApplication.class, args);
